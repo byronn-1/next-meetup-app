96f8142554af9e8b2bc32183232414e27b49eb8b
Byron Lee <byronn.lee@gmail.com>
2022-04-25 11:24:38 +0100

    added a dynamic route

---
+++ pages/dynamicRouteBasic/[newsId].js
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+
+function DetailPage() {
+    const router = useRouter();
+
+    console.log(router.query.newsId);
+
+    /*http://localhost:3001/dynamicRouteBasic/something*/
+    //note that this is all based on the name of the file in the brackets
+
+    return <h1>The Detail Page</h1>
+}
+
+export default DetailPage;
